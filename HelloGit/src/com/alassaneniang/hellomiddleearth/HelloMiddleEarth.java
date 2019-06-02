fc4a2de981af210f0dbd3dcbf0b158fcd6ca3b8f
Alassane NIANG <adniang75@gmail.com>
2019-06-02 23:10:57 +0200

    convert for loop to enhanced for loop

--- HelloGit/src/com/alassaneniang/hellomiddleearth/HelloMiddleEarth.java
+++ HelloGit/src/com/alassaneniang/hellomiddleearth/HelloMiddleEarth.java
@@ -7,8 +7,8 @@ public class HelloMiddleEarth {
         String[] names = { "Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf" };
 
         // print greetings
-        for ( int i = 0; i < names.length; i++ ) {
-            System.out.format( "Hello, %s!%n", names[i] );
+        for ( String name : names ) {
+            System.out.format( "Hello, %s!%n", name );
         }
 
     }
